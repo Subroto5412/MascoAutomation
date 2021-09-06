08717b92287af114a2440fcef7e73dd050987a45
DESKTOP-HS6DN56\Asus <subroto_mohonto@yahoo.com>
2021-09-06 13:29:29 +0600

    Splash screen code refactory

--- app/src/main/java/com/bd/mascogroup/automation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bd/mascogroup/automation/ui/splash/SplashActivity.kt
@@ -4,16 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.animation.AnimationUtils
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.databinding.library.baseAdapters.BR
 import com.bd.mascogroup.automation.R
 import com.bd.mascogroup.automation.databinding.ActivitySplashBinding
 import com.bd.mascogroup.automation.ui.base.BaseActivity
 import com.bd.mascogroup.automation.ui.login.LoginActivity
-
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -41,41 +37,9 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(), I
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.navigator = this
-       /* Handler().postDelayed(
-                {
-                    openMainActivity()
-                    finish()
-                }, 3 * 1000)*/
 
         val animationZoom = AnimationUtils.loadAnimation(this, R.anim.zoom)
         activity_spalsh_down_logo_im2.startAnimation(animationZoom)
-
-
-
-        /*if (density > 400) {
-            textNameAnimationXXHDI()
-        } else {
-            when (density) {
-                DisplayMetrics.DENSITY_LOW -> {
-                    textNameAnimation()
-                }
-                DisplayMetrics.DENSITY_MEDIUM -> {
-                    textNameAnimation()
-                }
-                DisplayMetrics.DENSITY_HIGH -> {
-                    textNameAnimation()
-                }
-                DisplayMetrics.DENSITY_XHIGH -> {
-                    textNameAnimation()
-                }
-                DisplayMetrics.DENSITY_XXHIGH -> {
-                    textNameAnimationXXHDI()
-                }
-                DisplayMetrics.DENSITY_XXXHIGH -> {
-                    textNameAnimationXXHDI()
-                }
-            }
-        }*/
 
         Handler().postDelayed(
                 {
@@ -83,18 +47,11 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(), I
                     activity_spalsh_down_logo_im2.startAnimation(animationZoom1)
                 }, 1000)
 
-       /*  Handler().postDelayed(
-                  {
-                      activity_spalsh_down_logo_im2.isGone = true
-                      activity_spalsh_down_logo_im3.isGone = false
-                  }, 500)
-*/
-
         Handler().postDelayed(
-                  {
-                      val animationZoom2 = AnimationUtils.loadAnimation(this, R.anim.zoom2)
-                      activity_spalsh_down_logo_im2.startAnimation(animationZoom2)
-                  }, 2000)
+                {
+                    val animationZoom2 = AnimationUtils.loadAnimation(this, R.anim.zoom2)
+                    activity_spalsh_down_logo_im2.startAnimation(animationZoom2)
+                }, 2000)
 
         Handler().postDelayed(
                 {
@@ -102,9 +59,6 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(), I
                     activity_spalsh_down_logo_im2.startAnimation(animationZoom3)
                 }, 3000)
 
-
-//        val animationZoom2 = AnimationUtils.loadAnimation(this, R.anim.zoom2)
-//        activity_spalsh_down_logo_im2.startAnimation(animationZoom2)
         Handler().postDelayed({
             viewModel.startSeeding()
             finish()
@@ -117,13 +71,8 @@ class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(), I
         startActivity(intent)
         finish()
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-     /*   val intent = Intent(this, MapsActivity::class.java)
-        startActivity(intent)*/
     }
 
-    //    override fun exitApplication() {
-//        finish()
-//    }
     override fun androidInjector(): AndroidInjector<Any>? {
         return fragmentDispatchingAndroidInjector
     }
