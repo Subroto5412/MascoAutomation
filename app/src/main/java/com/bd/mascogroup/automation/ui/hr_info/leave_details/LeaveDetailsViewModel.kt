869053a4ecf76b2bc04e053d15a4c71939d5aa6a
DESKTOP-HS6DN56\Asus <subroto_mohonto@yahoo.com>
2021-09-16 16:11:10 +0600

    icon for side menu

--- app/src/main/java/com/bd/mascogroup/automation/ui/hr_info/leave_details/LeaveDetailsViewModel.kt
+++ app/src/main/java/com/bd/mascogroup/automation/ui/hr_info/leave_details/LeaveDetailsViewModel.kt
@@ -160,7 +160,7 @@ class LeaveDetailsViewModel @Inject constructor(
                         )
                         spinnerArrayAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item) // The drop down view
                         fYearSpinner.setAdapter(spinnerArrayAdapter)
-                        
+
                         UtilMethods.hideLoading()
                     }, { error ->
                         UtilMethods.hideLoading()
